dee6f18f59c78e1a3715061ff9dd62e0573535c7
victormarcantonio <victor_marcantonio@hotmail.com>
2022-10-05 15:42:23 -0300

    domain event customer

---
+++ src/domain/event/customer/handler/envia-console-log.handler.ts
@@ -0,0 +1,12 @@
+import EventHandlerInterface from "../../@shared/event.handler.interface";
+import CustomerChangedEvent from "../customer-changed.event";
+import CustomerCreatedEvent from "../customer-created.event";
+
+
+export default class EnviaConsoleLogHandler
+  implements EventHandlerInterface<CustomerCreatedEvent>
+{
+  handle(event: CustomerChangedEvent): void {
+    console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData['address']['street']}, ${event.eventData.address.number}, ${event.eventData.address.zip}, ${event.eventData.address.city}`); 
+  }
+}
